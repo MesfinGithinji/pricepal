562ad75d7a4d182f12dca710ba5189857160e0b6
MesfinGithinji <meshgithinji@gmail.com>
2023-11-01 02:36:49 +0300

    Serch bar for the link to be scrapped

--- app/components/Searchbar.tsx
+++ app/components/Searchbar.tsx
@@ -1,4 +1,6 @@
 "use client"
+import { scrapeAndStoreProduct } from '@/lib/actions';
+import { scrapeAmazonProduct } from '@/lib/scrapper';
 import React, { FormEvent } from 'react'
 import { useState } from 'react';
 
@@ -9,8 +11,8 @@ const isValidAmazonProductURL = (url: string) => {
     const hostname = parsedURL.hostname; //parse the hostname of that url
     //we need to check if the url the user has entered has anything like amazon.com 
     if (hostname.includes('amazon.com') ||
-        hostname.includes('jumia.com') ||
-        hostname.includes('jiji.'))
+        hostname.includes('amazon.') ||
+        hostname.includes('amazon'))
         {
           return true;
         }
@@ -29,7 +31,7 @@ const Searchbar = () => {
 
     //we have to turn this into a client side rendered component
     // also handle the event once submit is triggered
-    const handleSubmit = (event: FormEvent<HTMLFormElement>)=> {
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>)=> {
       event.preventDefault();//prevent default behaviour of browser once its submitted
 
       const isValidLink = isValidAmazonProductURL(searchPrompt);
@@ -39,6 +41,8 @@ const Searchbar = () => {
         setIsLoading(true);
 
         //implement scrapping the product page
+        const product = await scrapeAndStoreProduct(searchPrompt);
+        
       } catch (error) {
         console.log(error);
       } finally {
