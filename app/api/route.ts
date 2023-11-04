8600b2c4ab8a6a179a652c5cc9f6ba83731d5b7d
MesfinGithinji <meshgithinji@gmail.com>
2023-11-05 00:58:43 +0300

    Added export variables

--- app/api/route.ts
+++ app/api/route.ts
@@ -7,7 +7,7 @@ import { scrapeAmazonProduct } from "@/lib/scrapper";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 
 export const maxDuration = 300; // function can run for a maximum of 300 seconds
-export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic"; //route segment config disabling all caching of fetch requests and always revalidating
 export const revalidate = 0;
 
 export async function GET(request: Request) {
