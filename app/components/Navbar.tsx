353b2b7a3bf0fdd26431fa3b692a58fb7c4c1514
MesfinGithinji <meshgithinji@gmail.com>
2023-11-13 14:17:01 +0300

    Final Name change

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = () => {
                     alt="logo"
                 />
                 <p className="nav-logo">
-                    Price<span className="text-primary">Dropp</span>
+                    Price<span className="text-primary">Pal</span>
                 </p>
             </Link>
             <div className="flex items-center gap-5">
